ad54e68c831c4cfa4a8420bd4fa9026d7bc8eee6
frankdew <frankdew23@gmail.com>
2016-08-12 11:55:46 +0200

    Add Cloudinary

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'carrierwave'
 gem 'rails_12factor', group: :production
+gem 'cloudinary'
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dbkvamybe'
+  config.api_key = '466915854735217'
+  config.api_secret = 'O4KhvHz1N9Hn0jTjW5RkbmtFc9Y'
+  config.cdn_subdomain = true
+end
